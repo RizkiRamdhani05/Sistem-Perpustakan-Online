01f4fc61913c82ee128f1a0288c9db9bc7d884a5
erdiansyahm <erdiansyah975@gmail.com>
2024-03-09 19:13:39 +0700

    menambahkan fitur buku

---
+++ src/Buku.java
@@ -0,0 +1,44 @@
+public class Buku {
+    private String judul;
+    private String penulis;
+    private int tahunTerbit;
+
+    // Konstruktor
+    public Buku(String judul, String penulis, int tahunTerbit) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    // Getter dan Setter
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getPenulis() {
+        return penulis;
+    }
+
+    public void setPenulis(String penulis) {
+        this.penulis = penulis;
+    }
+
+    public int getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    public void setTahunTerbit(int tahunTerbit) {
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    // Method tampilkanInfo
+    public void tampilkanInfo() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Penulis: " + penulis);
+        System.out.println("Tahun Terbit: " + tahunTerbit);
+    }
+}
